0d2c4498ba2e640050dbce7a04f0a57bcaf87bab
Keiran McDonald <keza@keza.net>
2019-03-28 20:55:17 +1000

    Add seconds remaining

--- index.js
+++ index.js
@@ -6,6 +6,54 @@ module.exports = function(homebridge) {
   homebridge.registerAccessory("homebridge-watergate", "Watergate", Watergate);
 };
 
+const secondsRemaining = (currentStartTime, durationSeconds) => {
+  const now = new Date();
+  const currentRunTime = (now - currentStartTime) / 1000;
+  const remaining = Math.round(durationSeconds - currentRunTime);
+  console.log({ currentStartTime });
+  console.log({ remaining });
+  if (remaining > 0) {
+    return remaining;
+  }
+  return 0;
+};
+
+var hw = require("./hardware.js");
+let timeoutHandle = null;
+let startTime = null;
+let durationSeconds = 300;
+
+const turnOff = valveService => {
+  startTime = null;
+  hw.off(3);
+  valveService.setCharacteristic(Characteristic.InUse, 0);
+};
+
+const updateOffTimer = (valveService, turnOffSeconds) => {
+  //todo turn off after x seconds
+  if (timeoutHandle) {
+    console.log("clearing timeout");
+    clearTimeout(timeoutHandle);
+  }
+  console.log(`turning off in ${turnOffSeconds} seconds`);
+  timeoutHandle = setTimeout(() => {
+    console.log("turning off");
+    valveService.setCharacteristic(Characteristic.Active, 0);
+    turnOff(valveService);
+  }, turnOffSeconds * 1000);
+};
+
+const turnOn = (valveService, durationSeconds) => {
+  startTime = new Date();
+  hw.on(3);
+  valveService.setCharacteristic(Characteristic.InUse, 1);
+  valveService.setCharacteristic(Characteristic.SetDuration, durationSeconds);
+
+  const remainingSeconds = secondsRemaining(startTime, durationSeconds);
+
+  updateOffTimer(valveService, remainingSeconds);
+};
+
 function Watergate(log, config) {
   this.log = log;
   this.name = config.name;
@@ -17,50 +65,31 @@ Watergate.prototype.getServices = function() {
   var valveService = new Service.Valve(plugin.name);
   valveService.getCharacteristic(Characteristic.ValveType).updateValue(1);
 
-  //var hw = require("./hardware.js");
-
-  let startTime = null;
   valveService
     .getCharacteristic(Characteristic.Active)
     .on("set", function(value, callback) {
-      plugin.log("Active -> " + value);
+      plugin.log("set Active " + value);
       if (value) {
-        startTime = new Date();
-        //   hw.on(3);
-        valveService.setCharacteristic(Characteristic.InUse, 1);
-        valveService.setCharacteristic(
-          Characteristic.SetDuration,
-          durationSeconds
-        );
-        //todo turn off after x seconds
+        turnOn(valveService, durationSeconds);
       } else {
-        startTime = null;
-        //   hw.off(3);
-        valveService.setCharacteristic(Characteristic.InUse, 0);
+        turnOff(valveService);
       }
       callback();
     })
     .on("get", function(callback) {
-      callback(null, startTime !== null);
+      const active = startTime !== null;
+      plugin.log("get Active " + active);
+      callback(null, active);
     });
 
-  const secondsRemaining = () => {
-    const now = new Date();
-    const currentRunTime = (now - startTime) / 1000;
-    const remaining = Math.round(durationSeconds - currentRunTime);
-    console.log({ remaining });
-    return remaining;
-  };
-
-  let durationSeconds = 300;
   valveService
     .addCharacteristic(Characteristic.SetDuration)
     .on("set", function(value, callback) {
-      plugin.log("SetDuration " + value);
+      plugin.log("set SetDuration " + value);
       durationSeconds = value;
       valveService.setCharacteristic(
         Characteristic.RemainingDuration,
-        secondsRemaining()
+        secondsRemaining(startTime, durationSeconds)
       );
 
       callback();
@@ -73,12 +102,13 @@ Watergate.prototype.getServices = function() {
   valveService
     .getCharacteristic(Characteristic.RemainingDuration)
     .on("set", function(value, callback) {
-      plugin.log("Remaining -> " + value);
+      plugin.log("set Remaining " + value);
+      updateOffTimer(valveService, value);
       callback();
     })
     .on("get", function(callback) {
-      const remaining = secondsRemaining();
-      plugin.log("get remaining " + remaining);
+      const remaining = secondsRemaining(startTime, durationSeconds);
+      plugin.log("get Remaining " + remaining);
       callback(null, remaining);
     });
 
